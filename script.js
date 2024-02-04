7bbc74ec4defddb2e8a2004f141ab9a52f7f8b75
jessikea <jessikea@jtran.git>
2024-02-04 17:47:16 -0500

    fixed linkedin

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// This script is used to switch between the different pages of the website
 document.querySelectorAll("header ul li").forEach(function(page) {
     page.onclick = () => {
         let pageName = page.id.split("-")[1];
@@ -5,12 +7,14 @@ document.querySelectorAll("header ul li").forEach(function(page) {
     }
 });
 
+// This function is used to show the page with the given name
 function showPage(pageName) {
     clearPages();
     var current_page = document.getElementById(pageName);
     current_page.style.display = "block";
 }
 
+// This function is used to hide all the pages
 function clearPages() {
     var pages = document.querySelectorAll("main > div");
     
